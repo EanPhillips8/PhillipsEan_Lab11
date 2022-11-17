ab1a0e68b15ef4c2b0e8f90cb49c23ce093db37d
EanPhillips8 <eanphillips8@gmail.com>
2022-11-17 18:10:07 -0500

    ouyg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+   static ArrayList<String> list = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in =new Scanner(System.in);
+
+final String Menu = "A- Add D- Delete P- Print Q- Quit";
+boolean done = false;
+String cmd = "";
+
+do {
+    displayList();
+    cmd = SafeInput.getRegExString(in,Menu,"[AaDdPpQq]");
+    cmd = cmd.toUpperCase();
+    String item="";
+    switch(cmd)
+    {
+        case"A":
+            list.add(SafeInput.getNonZerolength(in,"Enter your item to the list"));//prompt user to add item to list
+            //make sure it is not an empty string
+            //add it to list
+            break;
+        case"D":
+        list.remove(SafeInput.getRangedInt(in,"What item would you like to delete",1,100));
+            //Prompt user for the number of the item to delete
+            break;
+        case"P":
+            System.out.println(list); //print list
+            break;
+        case"Q":
+            System.exit(0);
+            break;
+    }
+    System.out.println("cmd is " + cmd);
+}while(!done);
+    }
+    private static void displayList()
+    {
+        System.out.println("---------------------------------------");
+        if(list.size()!= 0) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%3d%5s", i + 1, list.get(i));
+            }
+        }
+        else
+            System.out.println("------------ List is Empty ------------");
+
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,184 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.Scanner;
+
+
+public class SafeInput {
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+
+        int retVal = low - 1;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + "[" + low + "]" + "[" + high + "]: ");
+            if (pipe.hasNextInt())
+                retVal = pipe.nextInt();
+            pipe.nextLine();
+            if (retVal >= low && retVal <= high) {
+                // System.out.println("You said your favorite number is: " + retVal);
+                done = true;
+            } else {
+                System.out.println("You said your number is " + retVal + " but that is not in range.");
+            }
+
+        } while (!done);
+
+        return retVal;
+    }
+
+
+    public static int getInt(Scanner pipe, String prompt) {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            if (pipe.hasNextInt())
+                retVal = pipe.nextInt();
+            pipe.nextLine();
+            if (retVal >= 0.1) {
+                // System.out.println("You said your favorite number is: " + retVal);
+                done = true;
+            } else {
+                System.out.println("That is not a number.");
+            }
+
+        } while (!done);
+
+        return retVal;
+    }
+
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+
+        double retVal = low - 1;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + "[" + low + "]" + "[" + high + "]: ");
+            if (pipe.hasNextDouble())
+                retVal = pipe.nextDouble();
+            pipe.nextLine();
+            if (retVal >= low && retVal <= high) {
+                // System.out.println("You said your favorite number is: " + retVal);
+                done = true;
+            } else {
+                System.out.println("You entered " + trash + " but that is not a number.");
+            }
+
+        } while (!done);
+
+        return retVal;
+
+    }
+
+    public static double getDouble(Scanner pipe, String prompt) {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            if (pipe.hasNextDouble())
+                retVal = pipe.nextDouble();
+            pipe.nextLine();
+            if (retVal >= 0.1) {
+                // System.out.println("You said your favorite number is: " + retVal);
+                done = true;
+            } else {
+                System.out.println("Please enter a double.");
+            }
+
+        } while (!done);
+
+        return retVal;
+    }
+
+    public static String getRegExString(Scanner pipe, String prompt, String pattern) {
+
+        String retVal = "";
+        retVal = pipe.nextLine();
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+
+            if (retVal.matches(pattern)) {
+                done = true;
+            } else {
+                System.out.println("That does not match the pattern" + pattern);
+            }
+
+        } while (!done);
+
+        return retVal;
+
+    }
+
+    public static boolean getYN(Scanner pipe, String prompt) {
+
+        String respYN = "";
+        boolean retVal = false;
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + "[Y/N]: ");
+            respYN = pipe.nextLine();
+
+            if (respYN.equalsIgnoreCase("Y")) {
+                retVal = true;
+                done = true;
+            } else if (respYN.equalsIgnoreCase("N")) {
+                retVal = false;
+                done = true;
+            } else {
+                System.out.println("You must enter [Y/N]");
+            }
+        } while (!done);
+        return retVal;
+    }
+
+    public static String getNonZerolength(Scanner pipe, String prompt) {
+        String retVal = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+            if (retVal.length() > 0) {
+                done = true;
+            } else {
+                System.out.println("You have to enter at least one character: ");
+            }
+        } while (!done);
+
+        return retVal;
+    }
+
+
+
+    public static double CtoF(Scanner pipe, String prompt) {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do{
+            System.out.print(prompt + ": ");
+            if (pipe.hasNextDouble())
+                retVal = (pipe.nextDouble()*1.8)+32;
+            pipe.nextLine();
+            if (retVal >= 0.1) {
+                // System.out.println("You said your favorite number is: " + retVal);
+                done = true;
+            } else {
+                System.out.println("You said your favorite number is " + trash + " but that is not in range.");
+            }
+        }while(!done);
+
+        return retVal;
+
+    }
 }
